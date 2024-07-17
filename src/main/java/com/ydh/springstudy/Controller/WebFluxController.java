3f1ac05fc44566d85faa7d42d163db53958fd8b9
daehyeokyou <ydh4085@naver.com>
2024-07-17 17:33:45 +0900

    WebFlux Controller Creation (/w Annotation Type)

--- src/main/java/com/ydh/springstudy/Controller/WebFluxController.java
+++ src/main/java/com/ydh/springstudy/Controller/WebFluxController.java
@@ -1,9 +1,11 @@
 package com.ydh.springstudy.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -16,8 +18,25 @@ class WebFluxController {
         str = "Hello Webflux!";
     }
 
-    @GetMapping("/a")
-    public Mono<String> getAlertmanagerConfig() {
-        return Mono.just(str);
+    @GetMapping({"/mono/{val}",
+                "/mono"})
+    public Mono<String> getMono(@PathVariable(required = false) String val) {
+        if(val == null) {
+            System.out.println(this.str);
+            return Mono.just(this.str);
+        }
+        System.out.println(val);
+        return Mono.just(val);
+    }
+
+    @GetMapping({"/flux/{val}",
+            "/flux"})
+    public Flux<String> getFlux(@PathVariable(required = false) String val) {
+        if(val == null) {
+            System.out.println(this.str);
+            return Flux.just(this.str);
+        }
+        System.out.println(val);
+        return Flux.just(val);
     }
 }
--- src/main/java/com/ydh/springstudy/SpringstudyApplication.java
+++ src/main/java/com/ydh/springstudy/SpringstudyApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringstudyApplication {
-
+	/*
+	flatMapIterable
+	webclient
+	fromIterable
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstudyApplication.class, args);
 	}
